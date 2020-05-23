f0659f31dee1c35fe1ed7c85a56a217e0618d3d2
ysg <495743439@qq.com>
2020-05-23 21:37:28 +0800

    把本地工程添加到Git

---
+++ gittest1/src/com/ysg/git/test.java
@@ -0,0 +1,9 @@
+package com.ysg.git;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello Git");
+        System.out.println("hello Git");
+        System.out.println("hello Git");
+    }
+}
